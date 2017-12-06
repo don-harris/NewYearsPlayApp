3e9aa9b36c928f4fbb61ea1d2f9d2caec0406a48
don-harris <donald.walter.harris@gmail.com>
2017-12-06 15:28:22 +1300

    set up boiler plate

---
+++ test/client/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+import App from '../../client/components/App'
+import './setup-dom'
+
+App.prototype.componentDidMount = () => {}
+
+test('Hello World renders on App', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find('h1').text()).toBe('Hello World')
+})
